df30914235348a2aabdb8254552d519914aa4107
Mar <mar@strawmelonjuice.com>
2023-07-05 15:53:57 +0200

    update examples

--- test/example.js
+++ test/example.js
@@ -1,4 +1,11 @@
 var example = `
+<h3>Image 1</h3>
+<hl-img preview="https://strawmelonjuice.com/assets/img/media/art/MM2.png" alt="MouseMouse!" highlight="https://strawmelonjuice.com/assets/img/media/art/MM2.webp" href="https://strawmelonjuice.com/"></hl-img>
+
+<h3>Image 2</h3>
+<hl-img preview="https://strawmelonjuice.com/assets/img/media/art/MM2.png" alt="MouseMouse!" highlight="https://strawmelonjuice.com/assets/img/media/art/MM2.webp" style="height: 20px; bg-color: red"></hl-img>
+
+<h3>Image 3</h3>
 <hl-img preview="https://strawmelonjuice.com/assets/img/media/art/MM2.png" alt="MouseMouse!" highlight="https://strawmelonjuice.com/assets/img/media/art/MM2.webp"></hl-img>
-`
+`;
 document.getElementById("example").innerHTML = example;
