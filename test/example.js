d105e92c924c85ee1aec3996d45901ffdf20de07
Mar <mar@strawmelonjuice.com>
2023-07-05 16:00:24 +0200

    temp for #1

--- test/example.js
+++ test/example.js
@@ -4,6 +4,8 @@ var example = `
 
 <h3>Image 2</h3>
 <hl-img preview="https://strawmelonjuice.com/assets/img/media/art/MM2.png" alt="MouseMouse!" highlight="https://strawmelonjuice.com/assets/img/media/art/MM2.webp" style="height: 20px; bg-color: red"></hl-img>
+<hl-img preview="https://strawmelonjuice.com/assets/img/media/art/MM2.png" alt="MouseMouse!" highlight="https://strawmelonjuice.com/assets/img/media/art/MM2.webp" style="height: 20px; bg-color: red"></hl-img>
+<hl-img preview="https://strawmelonjuice.com/assets/img/media/art/MM2.png" alt="MouseMouse!" highlight="https://strawmelonjuice.com/assets/img/media/art/MM2.webp" style="height: 20px; bg-color: red"></hl-img>
 
 <h3>Image 3</h3>
 <hl-img preview="https://strawmelonjuice.com/assets/img/media/art/MM2.png" alt="MouseMouse!" highlight="https://strawmelonjuice.com/assets/img/media/art/MM2.webp"></hl-img>
