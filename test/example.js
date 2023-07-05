55f5bf8da500ba3a97c67904c040b85580e8aa0e
Mar <mar@strawmelonjuice.com>
2023-07-05 14:32:36 +0200

    Add tester

---
+++ test/example.js
@@ -0,0 +1,4 @@
+var example = `
+<hl-img preview="https://strawmelonjuice.com/assets/img/media/art/MM2.png" alt="MouseMouse!" highlight="https://strawmelonjuice.com/assets/img/media/art/MM2.webp"></hl-img>
+`
+document.getElementById("example").innerHTML = example;
