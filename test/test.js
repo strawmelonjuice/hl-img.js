a9657c224bffefff7ec5d534506db85268ea0b8d
Mar <just.mar.ok2813@gmail.com>
2023-07-14 15:20:13 +0000

    Update alt texts

--- test/test.js
+++ test/test.js
@@ -1,14 +1,14 @@
 var example = `
 <h3>Image 1</h3>
-<hl-img preview="../png/one.png" alt="MouseMouse!" style="width: 80%; max-width: 100%"></hl-img>
+<hl-img preview="../png/one.png" alt="Example image 1" style="width: 80%; max-width: 100%"></hl-img>
 
 <h3>Image 2</h3>
-<hl-img preview="../png/two.png" alt="Example image 2" highlight="../jpg/two.jpg" style="height: 20px; bg-color: red"></hl-img>
-<hl-img preview="../png/two.png" alt="Example image 2" highlight="../jpg/two.jpg" style="height: 20px; bg-color: red"></hl-img>
-<hl-img preview="../png/two.png" alt="Example image 2" highlight="../jpg/two.jpg" style="height: 20px; bg-color: red"></hl-img>
+<hl-img preview="../png/two.png" alt="Example image 2.1" highlight="../jpg/two.jpg" style="height: 20px; bg-color: red"></hl-img>
+<hl-img preview="../png/two.png" alt="Example image 2.2" highlight="../jpg/two.jpg" style="height: 20px; bg-color: red"></hl-img>
+<hl-img preview="../png/two.png" alt="Example image 2.3" highlight="../jpg/two.jpg" style="height: 20px; bg-color: red"></hl-img>
 
 <h3>Image 3</h3>
-<hl-img preview="../png/three.png" alt="MouseMouse!" highlight="../jpg/three.jpg" style="width: 80%" href="https://example.com/"></hl-img>
+<hl-img preview="../png/three.png" alt="Example image 3" highlight="../jpg/three.jpg" style="width: 80%" href="https://example.com/"></hl-img>
 `;
 const hlimg_options = {
       styling_closenotif_positioning: "bottom: 2em; right: 4em;",
