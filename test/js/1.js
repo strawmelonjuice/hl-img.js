c799ff855a73552931ecd6e9b6fe77186fe7ac3e
Mar <mar@strawmelonjuice.com>
2023-09-14 16:14:16 +0000

    Delete these

--- test/js/1.js
+++
@@ -1,7 +0,0 @@
-// // 3rd file to run.
-setTimeout(function () {
-    console.log("1.js is the 3rd file to run");
-    console.log("latest: " + window.parent.hlimgversions.latest + ", local: " + window.parent.hlimgversions.local)
-    document.getElementById("latestver").innerHTML = window.parent.hlimgversions.latest;
-    document.getElementById("localver").innerHTML = window.parent.hlimgversions.local;
-}, 1500);
