7405ca100c52af505a49092ddb6a4795c376ab7e
Mar <mar@strawmelonjuice.com>
2024-03-16 12:22:09 +0100

    to 2.1.0

--- src/hl-img.ts
+++ src/hl-img.ts
@@ -1,7 +1,7 @@
 if (typeof window.document === "undefined") {
 	throw new Error("HL-IMG is meant for usage in the browser!");
 }
-const hlimg_ver = "2.0.2";
+const hlimg_ver = "2.1.0";
 if (window.document.getElementsByClassName("hl-img")[0] !== null) {
 	const closenotif = window.document.createElement("div");
 	closenotif.innerHTML =
@@ -32,7 +32,27 @@ let hlimg_opt: {
 	styling_hlimg_maxheight: string;
 	timing_pop_location: number;
 };
-const hlimg_options_default = {
+interface hlimgoptions {
+	timing_fade_opacity: number;
+	timing_pop_location: number;
+	styling_closenotif_positioning: string;
+	styling_closenotif_colors_background: string;
+	styling_closenotif_colors_text: string;
+	styling_closenotif_border_thickness: string;
+	styling_closenotif_border_type: string;
+	styling_closenotif_border_color: string;
+	styling_closenotif_padding: string;
+	styling_imageshow_zIndex: number;
+	styling_imageshow_positioning: string;
+	styling_imageshow_colors_background: string;
+	styling_imageshow_border_thickness: string;
+	styling_imageshow_border_type: string;
+	styling_imageshow_border_color: string;
+	styling_imageshow_border_radius: string;
+	styling_hlimg_maxwidth: string;
+	styling_hlimg_maxheight: string;
+}
+const hlimg_options_default: hlimgoptions = {
 	timing_fade_opacity: 0.5,
 	timing_pop_location: 700,
 	styling_closenotif_positioning: "top: 2em; right: 2em;",
@@ -54,7 +74,7 @@ const hlimg_options_default = {
 	styling_hlimg_maxheight: "100%",
 };
 
-let hlimg_options;
+let hlimg_options: hlimgoptions;
 if (window.document.getElementById("hlimg-options") !== null) {
 	hlimg_options = JSON.parse(
 		window.document.getElementById("hlimg-options").textContent,
@@ -80,159 +100,141 @@ versionDec.id = "hl-img-version";
 versionDec.value = hlimg_ver;
 window.document.head.appendChild(versionDec);
 
+interface HLIMGElement extends HTMLImageElement {
+	imageshow?: (action: boolean, elem: HLIMGElement) => void;
+}
 const imoges = window.document.getElementsByTagName(
 	"hl-img",
 ) as HTMLCollectionOf<HTMLElement>;
 for (let i = imoges.length - 1; i >= 0; i--) {
+	// @ts-ignore
+	const pollepel: HTMLImageElement = imoges.item(i);
 	const newimogesid = `hl-img-${Math.floor(
 		Math.random() * (Math.floor(9999) - Math.ceil(1000) + 1) + Math.ceil(1000),
 	)}`;
-	const newimoges = window.document.createElement("img");
-	newimoges.alt = imoges.item(i).getAttribute("alt");
+	const newimoges: HLIMGElement = window.document.createElement("img");
+	newimoges.alt = pollepel.getAttribute("alt");
 	if (imoges !== null) {
-		if (imoges.item(i).getAttribute("style") != null) {
-			newimoges.setAttribute("style", imoges.item(i).getAttribute("style"));
+		if (pollepel.getAttribute("style") != null) {
+			newimoges.setAttribute("style", pollepel.getAttribute("style"));
 		}
-		if (imoges.item(i).getAttribute("height") != null) {
-			newimoges.setAttribute("height", imoges.item(i).getAttribute("height"));
-			newimoges.dataset.htmlheight = imoges.item(i).getAttribute("height");
+		if (pollepel.getAttribute("height") != null) {
+			newimoges.setAttribute("height", pollepel.getAttribute("height"));
+			newimoges.dataset.htmlheight = pollepel.getAttribute("height");
 		}
-		if (imoges.item(i).getAttribute("width") != null) {
-			newimoges.setAttribute("width", imoges.item(i).getAttribute("width"));
-			newimoges.dataset.htmlwidth = imoges.item(i).getAttribute("width");
+		if (pollepel.getAttribute("width") != null) {
+			newimoges.setAttribute("width", pollepel.getAttribute("width"));
+			newimoges.dataset.htmlwidth = pollepel.getAttribute("width");
 		}
 		// I make this typo a lot lmao
-		if (imoges.item(i).getAttribute("widht") != null) {
-			newimoges.setAttribute("width", imoges.item(i).getAttribute("widht"));
-			newimoges.dataset.htmlwidth = imoges.item(i).getAttribute("widht");
+		if (pollepel.getAttribute("widht") != null) {
+			newimoges.setAttribute("width", pollepel.getAttribute("widht"));
+			newimoges.dataset.htmlwidth = pollepel.getAttribute("widht");
 		}
-		if (imoges.item(i).style.height != null) {
-			newimoges.setAttribute("height", imoges.item(i).style.height);
-			newimoges.dataset.htmlheight = imoges.item(i).style.height;
+		if (pollepel.style.height != null) {
+			newimoges.setAttribute("height", pollepel.style.height);
+			newimoges.dataset.htmlheight = pollepel.style.height;
 		}
-		if (imoges.item(i).style.width != null) {
-			newimoges.setAttribute("width", imoges.item(i).style.width);
-			newimoges.dataset.htmlwidth = imoges.item(i).style.width;
+		if (pollepel.style.width != null) {
+			newimoges.setAttribute("width", pollepel.style.width);
+			newimoges.dataset.htmlwidth = pollepel.style.width;
 		}
-		if (imoges.item(i).getAttribute("class") != null) {
-			newimoges.setAttribute("class", imoges.item(i).getAttribute("class"));
+		if (pollepel.getAttribute("class") != null) {
+			newimoges.setAttribute("class", pollepel.getAttribute("class"));
 		}
-		if (imoges.item(i).getAttribute("id") != null) {
-			newimoges.setAttribute("id", imoges.item(i).getAttribute("id"));
+		if (pollepel.getAttribute("id") != null) {
+			newimoges.setAttribute("id", pollepel.getAttribute("id"));
 		} else {
 			newimoges.id = newimogesid;
 		}
 
-		newimoges.setAttribute("onclick", "this.imageshow(1, this)");
-		newimoges.imageshow = function (
-			action: boolean,
-			elem: {
-				id: string;
-				style: {
-					opacity: string;
-					cursor: string;
-					height: string;
-					width: string;
-				};
-				classList: {
-					add: (arg0: string) => void;
-					remove: (arg0: string) => void;
-				};
-				setAttribute: (arg0: string, arg1: string) => void;
-				dataset: {
-					href: null;
-					showsrc: string;
-					htmlheight: null;
-					htmlwidth: null;
-					prevsrc: string;
-				};
-				src: string;
-				title: string;
-				contains: (arg0: string) => string;
-			},
-		) {
-			if (action) {
-				console.log(`1/2 Zooming in to ${elem.id}`);
-				elem.style.opacity = "0%";
-				setTimeout(function () {
-					console.log(`2/2 Zooming in to ${elem.id}`);
-					elem.classList.add("imageshow");
-					elem.setAttribute("onclick", "");
-					if (elem.dataset.href != null) {
-						elem.setAttribute(
-							"onclick",
-							`window.open('${elem.dataset.href}');`,
-						);
-						closenotif.innerHTML = `<p>Click outside of the image to put it back in place! Or click on the image again to open <a href="${elem.dataset.href}" target="_blank">this link</a> in a new tab.</p>`;
-						elem.style.cursor = "pointer";
-					} else {
-						closenotif.innerHTML =
-							"<p>Click outside of the image to put it back in place!</p>";
-					}
-					elem.style.height = "";
-					elem.setAttribute("height", "");
-					elem.style.width = "";
-					elem.setAttribute("width", "");
-					elem.src = elem.dataset.showsrc;
-					elem.style.opacity = "100%";
-					elem.title = "";
-					closenotif.style.display = "block";
-				}, hlimg_opt.timing_pop_location);
-				const abortController = new AbortController();
-				window.addEventListener(
-					"click",
-					// rome-ignore lint/suspicious/noExplicitAny: <explanation>
-					function (e: { target: HTMLImageElement }) {
-						if (!elem.contains(e.target)) {
-							elem.imageshow(0, elem);
-							abortController.abort();
+		newimoges.setAttribute("onclick", "hlimg.imageshow(1, this)");
+		// @ts-ignore
+		window.hlimg = {
+			imageshow: (action: boolean, elem: HLIMGElement) => {
+				if (action) {
+					console.log(`1/2 Zooming in to ${elem.id}`);
+					elem.style.opacity = "0%";
+					setTimeout(() => {
+						console.log(`2/2 Zooming in to ${elem.id}`);
+						elem.classList.add("imageshow");
+						elem.setAttribute("onclick", "");
+						if (elem.dataset.href != null) {
+							elem.setAttribute(
+								"onclick",
+								`window.open('${elem.dataset.href}');`,
+							);
+							closenotif.innerHTML = `<p>Click outside of the image to put it back in place! Or click on the image again to open <a href="${elem.dataset.href}" target="_blank">this link</a> in a new tab.</p>`;
+							elem.style.cursor = "pointer";
+						} else {
+							closenotif.innerHTML =
+								"<p>Click outside of the image to put it back in place!</p>";
 						}
-					},
-					{ signal: abortController.signal },
-				);
-			} else {
-				console.log(`1/2 Zooming out of ${elem.id}`);
-				elem.style.opacity = "0%";
-				setTimeout(function () {
-					console.log(`2/2 Zooming out of ${elem.id}`);
-					elem.classList.remove("imageshow");
-					elem.setAttribute("onclick", "this.imageshow(1, this)");
-					elem.style.cursor = "";
-					if (elem.dataset.htmlheight != null) {
-						elem.style.height = elem.dataset.htmlheight;
-						elem.setAttribute("height", elem.dataset.htmlheight);
-					}
-					if (elem.dataset.htmlwidth != null) {
-						elem.style.width = elem.dataset.htmlwidth;
-						elem.setAttribute("width", elem.dataset.htmlwidth);
-					}
-					elem.src = elem.dataset.prevsrc;
-					elem.style.opacity = "100%";
-					elem.title = "Click to show image";
-					closenotif.style.display = "none";
-				}, hlimg_opt.timing_pop_location);
-			}
+						elem.style.height = "";
+						elem.setAttribute("height", "");
+						elem.style.width = "";
+						elem.setAttribute("width", "");
+						elem.src = elem.dataset.showsrc;
+						elem.style.opacity = "100%";
+						elem.title = "";
+						closenotif.style.display = "block";
+					}, hlimg_opt.timing_pop_location);
+					const abortController = new AbortController();
+					window.addEventListener(
+						"click",
+						(ev) => {
+							if (!elem.contains(ev.target as Node)) {
+								//@ts-ignore
+								window.hlimg.imageshow(false, elem);
+								abortController.abort();
+							}
+						},
+						{ signal: abortController.signal },
+					);
+				} else {
+					console.log(`1/2 Zooming out of ${elem.id}`);
+					elem.style.opacity = "0%";
+					setTimeout(() => {
+						console.log(`2/2 Zooming out of ${elem.id}`);
+						elem.classList.remove("imageshow");
+						elem.setAttribute("onclick", "hlimg.imageshow(1, this)");
+						elem.style.cursor = "";
+						if (elem.dataset.htmlheight != null) {
+							elem.style.height = elem.dataset.htmlheight;
+							elem.setAttribute("height", elem.dataset.htmlheight);
+						}
+						if (elem.dataset.htmlwidth != null) {
+							elem.style.width = elem.dataset.htmlwidth;
+							elem.setAttribute("width", elem.dataset.htmlwidth);
+						}
+						elem.src = elem.dataset.prevsrc;
+						elem.style.opacity = "100%";
+						elem.title = "Click to show image";
+						closenotif.style.display = "none";
+					}, hlimg_opt.timing_pop_location);
+				}
+			},
 		};
-		if (imoges.item(i).getAttribute("preview") != null) {
-			newimoges.dataset.prevsrc = imoges.item(i).getAttribute("preview");
-			newimoges.src = imoges.item(i).getAttribute("preview");
+		if (pollepel.getAttribute("preview") != null) {
+			newimoges.dataset.prevsrc = pollepel.getAttribute("preview");
+			newimoges.src = pollepel.getAttribute("preview");
 		} else {
-			newimoges.src = imoges.item(i).getAttribute("src");
-			newimoges.dataset.prevsrc = imoges.item(i).getAttribute("src");
+			newimoges.src = pollepel.getAttribute("src");
+			newimoges.dataset.prevsrc = pollepel.getAttribute("src");
 		}
-		if (imoges.item(i).getAttribute("highlight") != null) {
-			newimoges.dataset.showsrc = imoges.item(i).getAttribute("highlight");
+		if (pollepel.getAttribute("highlight") != null) {
+			newimoges.dataset.showsrc = pollepel.getAttribute("highlight");
 		} else {
 			newimoges.dataset.showsrc = newimoges.dataset.prevsrc;
 		}
-		if (imoges.item(i).getAttribute("href") != null) {
-			newimoges.dataset.href = imoges.item(i).getAttribute("href");
+		if (pollepel.getAttribute("href") != null) {
+			newimoges.dataset.href = pollepel.getAttribute("href");
 		}
 		newimoges.style.maxWidth = hlimg_opt.styling_hlimg_maxwidth;
 		newimoges.style.maxHeight = hlimg_opt.styling_hlimg_maxheight;
 		newimoges.title = "Click to show image";
 		newimoges.classList.add("hl-img");
-		imoges.item(i).parentNode.replaceChild(newimoges, imoges.item(i));
+		pollepel.parentNode.replaceChild(newimoges, pollepel);
 	}
 }
 
